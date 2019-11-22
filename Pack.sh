34e72e2d116c7d6e3e277914b08f4b503bb6c33b
yaoxin_work <779354187@qq.com>
2019-11-22 18:47:41 +0800

    发布脚本修改

--- Pack.sh
+++ Pack.sh
@@ -3,37 +3,34 @@
 # unpack
 
 # 路径设置  
-#QT_PATH  qt路径 
-#openBrowser_PATH  程序源码路径 
-#PATH  系统路径 
 export QT_PATH=/home/yc/Qt5.11.3/5.11.3/gcc_64         
-export openBrowser_PATH=/home/yc/Documents/work
+export openBrowser_PATH=/home/yc/Documents/QT/openBrowser
 export PATH=$QT_PATH/bin:$PATH
 
 
 #  判断是否有relese后生成的文件，如果有则删除之前打包好的文件
 # -f filename 如果 filename为常规文件，则为真
-if [ -f "$openBrowser_PATH/openBrowser/bin/openBrowser" ]
+if [ -f "$openBrowser_PATH/progrom/bin/openBrowser" ]
 then
 rm -rf "./openBrowser.AppImage"
-cp "$openBrowser_PATH/openBrowser/bin/openBrowser" "$openBrowser_PATH/App_openBrowser/usr/bin/openBrowser"
+cp "$openBrowser_PATH/progrom/bin/openBrowser" "$openBrowser_PATH/App/usr/bin/openBrowser"
 else
 echo "can not find openBrowser"
 exit 0
 fi
 
 #  删除之前生打包生成文件
-rm -rf "$openBrowser_PATH/App_openBrowser/AppRun"
-rm -rf "$openBrowser_PATH/App_openBrowser/.DirIcon"
-rm -rf "$openBrowser_PATH/App_openBrowser/usr/bin/qt.conf"
-rm -rf "$openBrowser_PATH/App_openBrowser/usr/lib"
-rm -rf "$openBrowser_PATH/App_openBrowser/usr/plugins"
-rm -rf "$openBrowser_PATH/App_openBrowser/usr/share/doc"
-rm -rf "$openBrowser_PATH/App_openBrowser/usr/translations"
-mkdir "$openBrowser_PATH/App_openBrowser/usr/lib"
+rm -rf "$openBrowser_PATH/App/AppRun"
+rm -rf "$openBrowser_PATH/App/.DirIcon"
+rm -rf "$openBrowser_PATH/App/usr/bin/qt.conf"
+rm -rf "$openBrowser_PATH/App/usr/lib"
+rm -rf "$openBrowser_PATH/App/usr/plugins"
+rm -rf "$openBrowser_PATH/App/usr/share/doc"
+rm -rf "$openBrowser_PATH/App/usr/translations"
+mkdir "$openBrowser_PATH/App/usr/lib"
 
 #  中文输入（fcitx），就是谷歌输入法里的之前移动到qt plugins里
-cp "$QT_PATH/plugins/platforminputcontexts/libfcitxplatforminputcontextplugin.so" "$openBrowser_PATH/App_openBrowser/usr/lib/libfcitxplatforminputcontextplugin.so"
+cp "$QT_PATH/plugins/platforminputcontexts/libfcitxplatforminputcontextplugin.so" "$openBrowser_PATH/App/usr/lib/libfcitxplatforminputcontextplugin.so"
 
 if [ -f "$QT_PATH/plugins/sqldrivers/libqsqlmysql.so" ] 
 then
@@ -46,7 +43,7 @@ mv "$QT_PATH/plugins/sqldrivers/libqsqlpsql.so" "$QT_PATH/plugins/sqldrivers/lib
 fi
 
 #  打包插件
-./linuxdeployqt.AppImage "$openBrowser_PATH/App_openBrowser/usr/bin/openBrowser" -appimage
+./linuxdeployqt.AppImage "$openBrowser_PATH/App/usr/bin/openBrowser" -appimage
 
 if [ -f "$QT_PATH/plugins/sqldrivers/libqsqlmysql.so.bak" ] 
 then
